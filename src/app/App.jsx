779c13ec9b8617c783684a55838c58ecf6e582f3
angelamariscotes <angela.mariscotes@lexmeet.com>
2026-06-14 04:28:23 +0800

    feat: contact

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import { Toaster } from 'react-hot-toast';
 
 function AppProviders({ children }) {
   return children;
@@ -8,6 +9,7 @@ function AppProviders({ children }) {
 export default function App() {
   return (
     <AppProviders>
+      <Toaster position="top-center" />
       <RouterProvider router={router} />
     </AppProviders>
   );
